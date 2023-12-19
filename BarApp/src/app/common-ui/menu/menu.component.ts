b3d67263c0c46767d1717030626f01e21ec5c237
franGiangiordano <frangiangiordano@gmail.com>
2023-12-19 11:52:05 -0300

    update menu

--- BarApp/src/app/common-ui/menu/menu.component.ts
+++ BarApp/src/app/common-ui/menu/menu.component.ts
@@ -1,31 +1,95 @@
 import { Component, OnInit } from '@angular/core';
-import { HostListener } from '@angular/core';
-import { Location } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { EntityListResponse } from '@common/models/entity.list.response';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { ProductType } from '@common/models/productType';
+import { ProductsTypeService } from 'src/app/projects/services/producsType-service/products-type.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
+  providers: [ProductsTypeService],
 })
 export class MenuComponent implements OnInit {
-  isMobile: boolean = false;
+  showBackButton: boolean = false;
 
-  constructor(private location: Location) {}
+  menuItems: any[] = [
+    { icon: 'person-outline', label: 'Iniciar Sesión', section: 'upper' },
+    { icon: 'calendar-outline', label: 'Reserva', section: 'upper' },
 
-  goBack(): void {
-    this.location.back();
-  }
+    { icon: 'mail-outline', label: 'Contacto', section: 'lower' },
+    { icon: 'help-outline', label: 'Preguntas Frecuentes', section: 'lower' },
+  ];
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.checkIfMobile();
+  accordionData: {
+    label: string;
+    icon: string;
+    subItems: { label: string }[];
+    expanded: boolean;
+  }[] = [
+    {
+      label: 'Carta',
+      icon: 'fast-food-outline',
+      subItems: [],
+      expanded: false,
+    },
+  ];
+
+  pageSize: number = 10;
+  pageIndex: number = 1;
+
+  productsTypeList$ = new Subject<EntityListResponse<ProductType>>();
+
+  constructor(
+    public producTypeService: ProductsTypeService,
+    public router: Router
+  ) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showBackButton = !(
+          event.url === '/home' || event.url === '/intro'
+        );
+      }
+    });
   }
 
   ngOnInit() {
-    this.checkIfMobile();
+    this.getProductsType();
   }
 
-  checkIfMobile() {
-    this.isMobile = window.innerWidth <= 768;
+  getProductsType(search?: string): void {
+    this.producTypeService
+      .getProductsType(this.pageIndex, this.pageSize, search)
+      .pipe(
+        map((response: any) => {
+          const products: { label: string }[] = response.results.map(
+            (producto: ProductType) => {
+              return {
+                label: producto.description,
+              };
+            }
+          );
+          return products;
+        })
+      )
+      .subscribe(transformedData => {
+        this.accordionData[0].subItems = transformedData;
+      });
+  }
+
+  /*para poder poner los items arriba o abajo del acordeon*/
+  getUpperMenuItems() {
+    return this.menuItems.filter(item => item.section === 'upper');
+  }
+
+  getLowerMenuItems() {
+    return this.menuItems.filter(item => item.section === 'lower');
+  }
+
+  goBack(): void {
+    window.history.back();
   }
 }
