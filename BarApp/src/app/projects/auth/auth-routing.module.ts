1a4c644ce81a5aa8b86c4986f67eee3c8a05ec29
gturconi <35380017+gturconi@users.noreply.github.com>
2023-12-20 10:01:07 -0300

    Update auth-routing.module.ts

--- BarApp/src/app/projects/auth/auth-routing.module.ts
+++ BarApp/src/app/projects/auth/auth-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+
 import { RegisterComponent } from "./register/register.component";
 import { LoginComponent } from "./login/login.component";
 import { AuthComponent } from "./auth.component/auth.component";
 import { ForgotComponent } from "./forgot/forgot.component";
 import { ProfileComponent } from "./profile/profile.component";
 import { EditComponent } from "./edit/edit.component";
+
+import { authGuard } from "@common/guards";
 
 const routes: Routes = [
   {
@@ -28,10 +31,12 @@ const routes: Routes = [
       {
         path: "profile",
         component: ProfileComponent,
+        canActivate: [authGuard],
       },
       {
         path: "profile/edit/:id",
         component: EditComponent,
+        canActivate: [authGuard],
       },
     ],
   },
