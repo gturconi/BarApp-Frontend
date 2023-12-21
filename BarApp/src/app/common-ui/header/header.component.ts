e6f3dc3c31651db40c893f822a2f3fe9b730651b
franGiangiordano <frangiangiordano@gmail.com>
2023-12-20 22:00:21 -0300

    hide header mobile and show menu admin mobile

--- BarApp/src/app/common-ui/header/header.component.ts
+++ BarApp/src/app/common-ui/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { LoginService } from '@common/services/login.service';
+import { UserRoles } from '@common/constants/user.roles.enum';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +11,22 @@ import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 export class HeaderComponent implements OnInit {
   showBackButton: boolean = false;
 
-  constructor(private router: Router, private cdr: ChangeDetectorRef) {
+  constructor(
+    private router: Router,
+    private cdr: ChangeDetectorRef,
+    private loginService: LoginService
+  ) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd || event instanceof NavigationStart) {
         this.updateBackButtonVisibility(this.router.url);
       }
     });
+  }
+
+  isAdmin(): boolean {
+    const userLoggedIn = this.loginService.isLoggedIn();
+    const userRole = userLoggedIn ? this.loginService.getUserRole() : null;
+    return userLoggedIn && userRole === UserRoles.Admin;
   }
 
   ngOnInit() {
