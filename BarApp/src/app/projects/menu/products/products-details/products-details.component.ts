a53296f97c5a80427ceeb17e9e1d0341d31b13a8
gturconi <35380017+gturconi@users.noreply.github.com>
2024-01-08 13:05:20 -0300

    Product details very soon

--- BarApp/src/app/projects/menu/products/products-details/products-details.component.ts
+++ BarApp/src/app/projects/menu/products/products-details/products-details.component.ts
@@ -1,14 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductsService } from "../services/products.service";
+import { Products } from "../models/products";
+import { ActivatedRoute } from "@angular/router";
+import { LoadingService } from "@common/services/loading.service";
+import { ImageService } from "@common/services/image.service";
+import { Avatar } from "@common/models/avatar";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-products-details',
-  templateUrl: './products-details.component.html',
-  styleUrls: ['./products-details.component.scss'],
+  selector: "app-products-details",
+  templateUrl: "./products-details.component.html",
+  styleUrls: ["./products-details.component.scss"],
 })
-export class ProductsDetailsComponent  implements OnInit {
+export class ProductsDetailsComponent implements OnInit {
+  product!: Products;
+  imagesUrl$!: Observable<string>;
 
-  constructor() { }
+  constructor(
+    private productsService: ProductsService,
+    private loadingService: LoadingService,
+    private imageService: ImageService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const typeId = params["idProd"];
+      if (typeId) {
+        this.doSearch(typeId);
+      }
+    });
+  }
 
+  async doSearch(id: string) {
+    const loading = await this.loadingService.loading();
+    await loading.present();
+    this.productsService.getProduct(id).subscribe(data => {
+      this.product = data;
+      this.imagesUrl$ = this.getImage(this.product);
+      loading.dismiss();
+    });
+  }
+
+  getImage(products: Products) {
+    const image = products.image as Avatar;
+    return this.imageService.getImage(image.data, image.type);
+  }
 }
