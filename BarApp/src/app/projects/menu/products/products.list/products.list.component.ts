f30bf219db56347aba327c4739a310f460429c79
franGiangiordano <frangiangiordano@gmail.com>
2024-01-09 22:39:02 -0300

    update style cards + sweetalert

--- BarApp/src/app/projects/menu/products/products.list/products.list.component.ts
+++ BarApp/src/app/projects/menu/products/products.list/products.list.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { Observable } from "rxjs";
-import { finalize } from "rxjs/operators";
-import { DELETE_OPTS } from "src/app/common/constants/messages.constant";
-import { Avatar } from "@common/models/avatar";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { DELETE_OPTS } from 'src/app/common/constants/messages.constant';
+import { Avatar } from '@common/models/avatar';
 
-import { ImageService } from "@common/services/image.service";
-import { LoadingService } from "@common/services/loading.service";
-import { LoginService } from "@common/services/login.service";
-import { ToastrService } from "ngx-toastr";
-import { ProductsService } from "../services/products.service";
-import { ProductsTypeService } from "../../products-type/services/products-type.service";
+import { ImageService } from '@common/services/image.service';
+import { LoadingService } from '@common/services/loading.service';
+import { LoginService } from '@common/services/login.service';
+import { ToastrService } from 'ngx-toastr';
+import { ProductsService } from '../services/products.service';
+import { ProductsTypeService } from '../../products-type/services/products-type.service';
 
-import { Products } from "../models/products";
+import { Products } from '../models/products';
 
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: "app-products.list",
-  templateUrl: "./products.list.component.html",
-  styleUrls: ["./products.list.component.scss"],
+  selector: 'app-products.list',
+  templateUrl: './products.list.component.html',
+  styleUrls: ['./products.list.component.scss'],
 })
 export class ProductsListComponent implements OnInit {
   productsList!: Products[];
@@ -27,7 +27,7 @@ export class ProductsListComponent implements OnInit {
   admin: boolean = false;
   showData: boolean = false;
   hide: boolean = true;
-  category: string | undefined = "";
+  category: string | undefined = '';
 
   constructor(
     private loginService: LoginService,
@@ -47,7 +47,7 @@ export class ProductsListComponent implements OnInit {
 
   getAndSearchByType(): void {
     this.route.params.subscribe(params => {
-      const typeId = params["idCat"];
+      const typeId = params['idCat'];
       if (typeId) {
         this.productsTypeService.getProductsType(typeId).subscribe(data => {
           this.category = data.description;
@@ -58,12 +58,12 @@ export class ProductsListComponent implements OnInit {
   }
 
   redirectToProductsEdit(productId: string) {
-    this.router.navigate(["edit/", productId], {
+    this.router.navigate(['edit/', productId], {
       relativeTo: this.route.parent,
     });
   }
   redirectToDetails(productId: string) {
-    this.router.navigate(["details/", productId], {
+    this.router.navigate(['details/', productId], {
       relativeTo: this.route.parent,
     });
   }
@@ -76,6 +76,16 @@ export class ProductsListComponent implements OnInit {
         this.productsList = data.results;
         this.setImages(this.productsList);
         this.showData = true;
+        if (this.productsList.length === 0) {
+          Swal.fire({
+            icon: 'info',
+            title: 'No hay productos',
+            text: 'Aun no hay productos cargados para esta categoría.',
+          }).then(() => {
+            this.router.navigate(['/menu/categories']); // Redirecciona al menú de categorías
+          });
+          this.showData = false;
+        }
       });
     } finally {
       loading.dismiss();
@@ -102,7 +112,7 @@ export class ProductsListComponent implements OnInit {
           .deleteProducts(id)
           .pipe(finalize(() => loading.dismiss()))
           .subscribe(() => {
-            this.toastrService.success("Producto eliminado");
+            this.toastrService.success('Producto eliminado');
             loading.dismiss();
             this.getAndSearchByType();
           });
