5f19da012f42e3d417e89828fa6070d46ce77010
gturconi <35380017+gturconi@users.noreply.github.com>
2024-01-14 18:40:13 -0300

    Profile Margin

--- BarApp/src/app/app.component.ts
+++ BarApp/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { LoginService } from '@common/services/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   showMenu: boolean = false;
+  addMarginBottom: boolean = false;
 
-  constructor() {}
+  constructor(private router: Router, private loginService: LoginService) {}
+
+  ngOnInit() {
+    const isAdmin = this.loginService.isAdmin();
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.addMarginBottom = event.url === '/auth/profile' && !isAdmin;
+      }
+    });
+  }
 
   onMenuToggle(showMenu: boolean) {
     this.showMenu = showMenu;
