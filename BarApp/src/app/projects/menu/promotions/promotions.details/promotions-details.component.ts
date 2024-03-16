69619d5445904e366a172971b8b2c4111029c159
franGiangiordano <frangiangiordano@gmail.com>
2024-03-16 16:21:45 -0300

    update promotions details

--- BarApp/src/app/projects/menu/promotions/promotions.details/promotions-details.component.ts
+++ BarApp/src/app/projects/menu/promotions/promotions.details/promotions-details.component.ts
@@ -104,15 +104,48 @@ export class PromotionsDetailsComponent implements OnInit {
   }
 
   isPromotionValid(promotion: Promotion): boolean {
-    if (
-      promotion.valid_from === undefined ||
-      promotion.valid_to === undefined
-    ) {
-      return false;
-    }
     const currentDate = this.getCurrentDate();
-    const validFrom = new Date(promotion.valid_from);
-    const validTo = new Date(promotion.valid_to);
+
+    if (
+      (!promotion.valid_from || !promotion.valid_to) &&
+      this.isCurrentDayOfWeekValid(promotion)
+    ) {
+      return true;
+    }
+
+    if (promotion.valid_from && promotion.valid_to) {
+      const validFrom = new Date(promotion.valid_from);
+      const validTo = new Date(promotion.valid_to);
+      if (
+        currentDate >= validFrom &&
+        currentDate <= validTo &&
+        !this.hasDays(promotion)
+      ) {
+        return true;
+      }
+      return (
+        currentDate >= validFrom &&
+        currentDate <= validTo &&
+        this.isCurrentDayOfWeekValid(promotion)
+      );
+    }
+
+    return false;
+  }
+
+  isCurrentDayOfWeekValid(promotion: Promotion): boolean {
+    const currentDayOfWeek = new Date().getDay();
+    return promotion.days_of_week?.includes(currentDayOfWeek) || false;
+  }
+
+  hasDays(promotion: Promotion) {
+    return promotion.days_of_week && promotion.days_of_week.length > 0;
+  }
+
+  isCurrentDateInRange(startDate: Date, endDate: Date): boolean {
+    const currentDate = this.getCurrentDate();
+    const validFrom = new Date(startDate);
+    const validTo = new Date(endDate);
     return currentDate >= validFrom && currentDate <= validTo;
   }
 
